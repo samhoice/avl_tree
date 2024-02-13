e57b0980e610590a5ab44a7da036a2775fb7be54
Jon Adelson <jon@adelson.io>
2024-02-13 14:41:47 -0500

    Adds tests and fixes insert and traverse

--- src/avl.py
+++ src/avl.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from typing import Optional, Any
 from enum import Enum
 
+
 class Order(Enum):
     IN = 0
     PRE = 1
@@ -9,31 +10,55 @@ class Order(Enum):
 
 
 class AVLNode:
-    """ Initially implementing a BST
+    """Initially implementing a BST
 
     This will be slowly updated to impment an AVL tree.
     """
-    def __init__(self, value: Any, left=None, right=None):
-        self._left = left
-        self._right = right
+
+    def __init__(
+        self,
+        value: Any = None,
+    ):
+        # TODO: If we can pass a value for the current node, then we have
+        # to expect None children. We shouldn't be able to pass values for
+        # the children here.
+
+        self._left = None
+        self._right = None
         self._value = value
 
+    def _init_children(self):
+        """create children
+
+        Can't do this in init because we don't want to call init
+        recursively.
+        """
+        if not self._left:
+            self._left = AVLNode()
+        if not self._right:
+            self._right = AVLNode()
+
     def insert(self, value: Any):
-        """ insert a node into the tree
+        """insert a node into the tree
 
         Could probably return +1 if the tree gets taller or 0 otherwise?
         Probably need to maintain a height
         """
-        if value > self._value:
-            if self._right:
+        if not self._value:
+            self._value = value
+            self._init_children()
+        elif value > self._value:
+            if not self._right:
+                # This shouldn't happen
+                print("WARNING! NO RIGHT CHILD")
+            else:
                 self._right.insert(value)
-            else:
-                self._right = AVLNode(value, None, None)
         elif value < self._value:
-            if self._left:
-                self._left.insert(value)
+            if not self._left:
+                # This shouldn't happen
+                print("WARNING! NO LEFT CHILD")
             else:
-                self._left = AVLNode(value, None, None)
+                self._left.insert(value)
         else:
             # FIXME: Drop dupes for now
             pass
@@ -41,33 +66,36 @@ class AVLNode:
     def __str__(self):
         return f"Node: {self._value} - left: {self._left}, right: {self._right}"
 
-    def traverse(self, function, parent = None, order = Order.IN):
-        """ recursively traverse the tree structure
-        """
-        if order == Order.PRE:
-            function(self._value, parent)
-        if self._left:
-            self._left.traverse(function, self, order)
-        if order == Order.IN:
-            function(self._value, parent)
-        if self._right:
-            self._right.traverse(function, self, order)
-        if order == Order.POST:
-            function(self._value, parent)
+    @classmethod
+    def traverse(cls, node, function, parent=None, order=Order.IN):
+        """recursively traverse the tree structure"""
 
+        if not node:
+            return
+
+        if node._value:
+            if order == Order.IN:
+                cls.traverse(node._left, function)
+                function(node._value)
+                cls.traverse(node._right, function)
+            elif order == Order.PRE:
+                function(node._value)
+                cls.traverse(node._left, function)
+                cls.traverse(node._right, function)
+            else:
+                cls.traverse(node._left, function)
+                cls.traverse(node._right, function)
+                function(node._value)
 
     def rotate_left(self):
         pass
 
-    
     def rotate_right(self):
         pass
 
-
     def delete(self, value=None):
-        """ remove a node from the tree
+        """remove a node from the tree
 
         match value and remove?
         """
         pass
-
--- tests/test_avl.py
+++ tests/test_avl.py
@@ -1,17 +1,70 @@
 # pytest
-from avl import AVLNode
+from avl import AVLNode, Order
 
 def test_tests():
     assert True
+
+def check_result(result, test_data):
+
+    assert(len(result) == len(test_data))
+    assert(sorted(result) == sorted(test_data))
+
 
 def test_empty_insert():
     tree = AVLNode()
 
     tree.insert(5)
     
-    list_version = []
-    tree.traverse(lambda x: list_version.append(x))
+    result = []
+    AVLNode.traverse(tree, lambda x: result.append(x))
 
-    assert(len(list_version) == 1)
-    assert(list_version[0] == 5)
-    
+    check_result(result, [5])
+
+def test_lesser_insert():
+    tree = AVLNode()
+
+    tree.insert(5)
+    tree.insert(4)
+
+    result = []
+    AVLNode.traverse(tree, lambda x: result.append(x))
+
+    check_result(result, [4,5])
+
+def test_greater_insert():
+    tree = AVLNode()
+
+    tree.insert(5)
+    tree.insert(6)
+
+    result = []
+    AVLNode.traverse(tree, lambda x: result.append(x))
+
+    check_result(result, [5, 6])
+
+
+def test_traversal_order():
+    """traversal in various order
+
+    TODO: Will need to be updated for more inserts
+    """
+    tree = AVLNode()
+
+    tree.insert(5)
+    tree.insert(6)
+    tree.insert(4)
+
+    result = []
+    AVLNode.traverse(tree, lambda x: result.append(x))
+
+    check_result(result, [4, 5, 6])
+
+    result = []
+    AVLNode.traverse(tree, lambda x: result.append(x), Order.PRE)
+
+    check_result(result, [5, 4, 6])
+
+    result = []
+    AVLNode.traverse(tree, lambda x: result.append(x), Order.POST)
+
+    check_result(result, [4, 6, 5])
